ea89ba3b9963bee01bc5c23fb55e091469a829fe
Piotr Moskała <moskala.p@eplan.pl>
2019-07-11 21:34:18 +0200

    Remove https from localhost development

--- FamilyGuy.Infrastructure/BaseUrl.cs
+++ FamilyGuy.Infrastructure/BaseUrl.cs
@@ -2,10 +2,6 @@
 {
     public class BaseUrl
     {
-        public static string Current
-        {
-            get;
-            set;
-        }
+        public static string Current { get; set; }
     }
 }
