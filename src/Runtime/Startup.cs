beebfad0922c6feb4e7731b459ce012cf4378460
Piotr Moskała <moskala.p@eplan.pl>
2019-07-13 12:25:12 +0200

    Add swagger API documentation framework

--- src/Runtime/Startup.cs
+++ src/Runtime/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 
 namespace FamilyGuy
@@ -28,10 +29,16 @@ namespace FamilyGuy
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddControllersAsServices();
 
+            AddSwaggerApiDocumentationFramework(services);
             ConfigureApiVersioningFramework(services);
 
             IContainer container = ConfigureAutofacDiContainer(services);
             return new AutofacServiceProvider(container);
+        }
+
+        private static void AddSwaggerApiDocumentationFramework(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "FamilyGuy API", Version = "v1" }); });
         }
 
         private static IContainer ConfigureAutofacDiContainer(IServiceCollection services)
@@ -60,6 +67,12 @@ namespace FamilyGuy
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NicePress Api v1");
+            });
 
             app.UseMvc();
         }
