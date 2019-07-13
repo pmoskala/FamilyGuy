1f6058e008a6eeb6ac8c72cde9e853800be9747b
Piotr Moskała <moskala.p@eplan.pl>
2019-07-13 12:20:51 +0200

    Add basic API endpoint

---
+++ src/API/FamilyGuy.UserApi/Controllers/AccountsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyGuy.UserApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class AccountsController : Controller
+    {
+        public AccountsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAccount()
+        {
+            return Ok();
+        }
+    }
+}
