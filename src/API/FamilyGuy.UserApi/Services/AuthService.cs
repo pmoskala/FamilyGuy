8265c28957f4ad36a2204d5e6ff33bfee147440f
Piotr Moskała <moskala.p@eplan.pl>
2019-08-27 10:55:31 +0200

    Jwt sub change

--- src/API/FamilyGuy.UserApi/Services/AuthService.cs
+++ src/API/FamilyGuy.UserApi/Services/AuthService.cs
@@ -35,23 +35,23 @@ namespace FamilyGuy.UserApi.Services
             AccountReadModel account = await _query.Query<Task<AccountReadModel>, UserAuthenticationModel>(userAuthenticationModel);
             if (account == null) return null;
 
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
             byte[] key = Encoding.ASCII.GetBytes(_jwtSettings.Key);
             SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new[]
                 {
-                    new Claim(ClaimTypes.Name, account.Name)
+                    new Claim(JwtRegisteredClaimNames.Sub, account.Id.ToString())
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
-            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
 
             return new AuthenticatedUserReadModel
             {
                 Token = tokenHandler.WriteToken(token),
-                Id = account.Id,
                 Name = account.Name,
                 Surname = account.Surname
             };
@@ -61,7 +61,6 @@ namespace FamilyGuy.UserApi.Services
     public class AuthenticatedUserReadModel
     {
         public string Token { get; set; }
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
     }
